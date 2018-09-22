a79f1e6db0f2762394793594a82505307c933fb5
Hiren Patel <hiren@freshbits.in>
2018-09-22 15:17:48 +0530

    Remove unused code from app js file

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,6 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -15,7 +14,6 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 const app = new Vue({
     el: '#app'
