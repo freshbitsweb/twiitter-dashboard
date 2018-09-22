34d398acce3a633df2e2ff366c0a8a76d3021b19
Hiren Patel <hiren@freshbits.in>
2018-09-22 15:18:40 +0530

    Make functionality of pusher for new twit

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,7 +14,37 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-
 const app = new Vue({
-    el: '#app'
+    el: '#application',
+
+    data: {
+        twit: {
+            text: 'હજી કય મડુ નથિ ભુરા.',
+            created_at: '',
+        }
+    },
+
+    methods: {
+        updateTwitFontSize: function () {
+            var numberOfTwitCharecter = document.getElementById('twiter-user-twit');
+
+            if (this.twit.text.length > 200) {
+                numberOfTwitCharecter.style.fontSize = '40px';
+            } else if (this.twit.text.length > 80) {
+                numberOfTwitCharecter.style.fontSize = '70px';
+            } else {
+                numberOfTwitCharecter.style.fontSize = '100px';
+            }
+        }
+    },
+
+    created: function () {
+        window.Echo.channel('new-twit-channel').listen('NewTwitRecived', (e) => {
+            this.twit.text = e.twit.text;
+            this.twit.created_at = e.twit.created_at;
+            this.updateTwitFontSize();
+        });
+
+        this.updateTwitFontSize();
+    }
 });
