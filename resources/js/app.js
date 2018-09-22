4d96819febc2570563f11cf2ef0066cfbeb841d4
Hiren Patel <hiren@freshbits.in>
2018-09-22 15:45:04 +0530

    Code refactoring

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,19 +21,18 @@ const app = new Vue({
         twit: {
             text: 'હજી કય મડુ નથિ ભુરા.',
             created_at: '',
+            fontSize: 100,
         }
     },
 
-    methods: {
-        updateTwitFontSize: function () {
-            var numberOfTwitCharecter = document.getElementById('twiter-user-twit');
-
-            if (this.twit.text.length > 200) {
-                numberOfTwitCharecter.style.fontSize = '40px';
-            } else if (this.twit.text.length > 80) {
-                numberOfTwitCharecter.style.fontSize = '70px';
+    watch: {
+        'twit.text': function (text) {
+            if (text.length > 200) {
+                this.twit.fontSize = 40;
+            } else if (text.length > 80) {
+                this.twit.fontSize = 70;
             } else {
-                numberOfTwitCharecter.style.fontSize = '100px';
+                this.twit.fontSize = 100;
             }
         }
     },
@@ -42,9 +41,6 @@ const app = new Vue({
         window.Echo.channel('new-twit-channel').listen('NewTwitRecived', (e) => {
             this.twit.text = e.twit.text;
             this.twit.created_at = e.twit.created_at;
-            this.updateTwitFontSize();
         });
-
-        this.updateTwitFontSize();
     }
 });
