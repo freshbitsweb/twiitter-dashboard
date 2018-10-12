ba43dd3bb3973ceb2512613b28a44f4a7f02a8d1
Hiren <hiren@freshbits.in>
2018-10-12 16:43:07 +0530

    Fetch latest tweet from the users list

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use TwitterAPIExchange;
+
+class HomeController extends Controller
+{
+    /**
+     * Display user latest tweet
+     *
+     * @return \Illuminate\Http\Response
+     **/
+    public function index()
+    {
+        $userTimeline = getUserLatestTweet();
+
+        $tweet = [
+            'text' => $userTimeline[0]->text,
+            'user_name' => $userTimeline[0]->user->name,
+            'created_at' => $userTimeline[0]->created_at,
+        ];
+
+        return view('welcome', compact('tweet'));
+    }
+
+    /**
+     * Returns the user timeline latest tweet
+     *
+     * @return Json user timeline latest tweet
+     **/
+    public function getUserLatestTweet()
+    {
+        $followedUserIds = explode(',', config('services.twitter.follow_user_id'));
+
+        $twitter = new TwitterAPIExchange($this->getTwitterSettings());
+        $userTimeline = $twitter
+            ->buildOauth('https://api.twitter.com/1.1/statuses/user_timeline.json', 'GET')
+            ->setGetfield("?user_id=" . $followedUserIds[0] . "&count=1")
+            ->performRequest()
+        ;
+
+        return json_decode($userTimeline);
+    }
+
+    /**
+     * Returns the twitter api settings
+     *
+     * @return array
+     **/
+    public function getTwitterSettings()
+    {
+        return [
+            'consumer_key' => config('twitter.consumer_key'),
+            'consumer_secret' => config('twitter.consumer_secret'),
+            // These two can be left empty since we'll only read from the Twitter's timeline
+            'oauth_access_token' => '',
+            'oauth_access_token_secret' => '',
+        ];
+    }
+}
